12b0473c7cedc95d2956fc75261dea4c2f06a4b5
yuanshuai <yuanshuai@bdall.com>
2019-08-16 09:45:37 +0800

    添加了employees表-devise需要用

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_21_025640) do
+ActiveRecord::Schema.define(version: 2019_08_15_090846) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "content", null: false
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "employees", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "mobile", default: "", null: false
+    t.integer "kind", default: 0, null: false
+    t.integer "flg", default: 0, null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_employees_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_employees_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_employees_on_unlock_token", unique: true
   end
 
   create_table "users", force: :cascade do |t|
